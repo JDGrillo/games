8b22666c8718c0e87787efae53671f6832d0ac81
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2019-10-24 18:55:41 -0500

    finished tictactoe

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -2,12 +2,14 @@ let content = document.getElementById('content');
 
 // STATE
 let ticTacToe = [
-    ["-","-","-"],
-    ["-","-","-"],
-    ["-","-","-"]
-    ]
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+]
 
 let isXTurn = true
+let moves = 0
+let over = false
 
 content.innerHTML = renderGame(ticTacToe);
 
@@ -19,16 +21,48 @@ function buttonClicked(event) {
     renderSquare(rowIdx, colIdx)
 }
 
+function hasSomeoneWon() {
+    let conditions = [
+        document.getElementById("00").innerHTML + document.getElementById("01").innerHTML + document.getElementById("02").innerHTML,
+        document.getElementById("10").innerHTML + document.getElementById("11").innerHTML + document.getElementById("12").innerHTML,
+        document.getElementById("20").innerHTML + document.getElementById("21").innerHTML + document.getElementById("22").innerHTML,
+        document.getElementById("01").innerHTML + document.getElementById("11").innerHTML + document.getElementById("21").innerHTML,
+        document.getElementById("02").innerHTML + document.getElementById("12").innerHTML + document.getElementById("22").innerHTML,
+        document.getElementById("00").innerHTML + document.getElementById("11").innerHTML + document.getElementById("22").innerHTML,
+        document.getElementById("02").innerHTML + document.getElementById("11").innerHTML + document.getElementById("20").innerHTML,
+    ]
+
+    if (moves < 9 && over === false) {
+        for (let i = 0; i < conditions.length; i++) {
+            if (conditions[i] === "XXX") {
+                document.getElementById("titleCard").innerHTML = "X WON!"
+                over = true
+            }
+            else if (conditions[i] === "OOO") {
+                document.getElementById("titleCard").innerHTML = "O WON!"
+                over = true
+            }
+        }
+    }
+    else {
+        document.getElementById("titleCard").innerHTML = "NOONE WON!"
+        over = true
+    }
+}
+
 function renderSquare(row, col) {
-    let squareID = row + col
-    console.log(squareID)
-    console.log(document.getElementById(squareID).innerHTML)
-    if (document.getElementById(squareID).innerHTML === "-") {
-        isXTurn === true ? character = "X" : character = "O"
-        document.getElementById(squareID).innerHTML = character
-        isXTurn === false ? isXTurn = true : isXTurn = false
-        isXTurn === true ? character = "X" : character = "O"
-        document.getElementById("titleCard").innerHTML =  `${character}'s Turn!`
+    if (over === false) {
+        let squareID = row + col
+        if (document.getElementById(squareID).innerHTML === "-") {
+            isXTurn === true ? character = "X" : character = "O"
+            document.getElementById(squareID).innerHTML = character
+            isXTurn === false ? isXTurn = true : isXTurn = false
+            isXTurn === true ? character = "X" : character = "O"
+            document.getElementById("titleCard").innerHTML = `${character}'s Turn!`
+            moves += 1
+            
+        }
+        hasSomeoneWon()
     }
 }
 
@@ -39,9 +73,9 @@ function renderGame(game) {
         <div class="container d-flex flex-column justify-content-start align-items-center">
             <h4 id="titleCard">X's Turn!</h4>
             <div class="w-50 text-center">
-                <button id="00" onClick = "buttonClicked(event)">${game[0][0]} </button>
-                <button id="01" onClick = "buttonClicked(event)">${game[0][1]} </button>
-                <button id="02" onClick = "buttonClicked(event)">${game[0][2]} </button>
+                <button id="00" onClick = "buttonClicked(event)">${game[0][0]}</button>
+                <button id="01" onClick = "buttonClicked(event)">${game[0][1]}</button>
+                <button id="02" onClick = "buttonClicked(event)">${game[0][2]}</button>
             </div>
             <div class="w-50 text-center">
                 <button id="10" onClick = "buttonClicked(event)">${game[1][0]}</button>

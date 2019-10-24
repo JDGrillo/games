260d90081a7165d739e0ce7e474fb18c87fd28ad
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2019-10-24 18:20:46 -0500

    adding files

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,32 +1,57 @@
-var content = document.getElementById('content');
+let content = document.getElementById('content');
 
-var ticTacToe = "Replace this with your own abstraction of Tic Tac Toe"
+// STATE
+let ticTacToe = [
+    ["-","-","-"],
+    ["-","-","-"],
+    ["-","-","-"]
+    ]
+
+let isXTurn = true
 
 content.innerHTML = renderGame(ticTacToe);
 
+function buttonClicked(event) {
+    let clickedButton = event.target
+    let clickedButtonID = clickedButton.id
+    let rowIdx = clickedButtonID[0]
+    let colIdx = clickedButtonID[1]
+    renderSquare(rowIdx, colIdx)
+}
 
-
+function renderSquare(row, col) {
+    let squareID = row + col
+    console.log(squareID)
+    console.log(document.getElementById(squareID).innerHTML)
+    if (document.getElementById(squareID).innerHTML === "-") {
+        isXTurn === true ? character = "X" : character = "O"
+        document.getElementById(squareID).innerHTML = character
+        isXTurn === false ? isXTurn = true : isXTurn = false
+        isXTurn === true ? character = "X" : character = "O"
+        document.getElementById("titleCard").innerHTML =  `${character}'s Turn!`
+    }
+}
 
 function renderGame(game) {
     // Change this render function to use the "game" parameter
 
     return `
         <div class="container d-flex flex-column justify-content-start align-items-center">
-            <h4>It's player O's turn!</h4>
+            <h4 id="titleCard">X's Turn!</h4>
             <div class="w-50 text-center">
-                <button>X</button>
-                <button>O</button>
-                <button>X</button>
+                <button id="00" onClick = "buttonClicked(event)">${game[0][0]} </button>
+                <button id="01" onClick = "buttonClicked(event)">${game[0][1]} </button>
+                <button id="02" onClick = "buttonClicked(event)">${game[0][2]} </button>
             </div>
             <div class="w-50 text-center">
-                <button>-</button>
-                <button>-</button>
-                <button>-</button>
+                <button id="10" onClick = "buttonClicked(event)">${game[1][0]}</button>
+                <button id="11" onClick = "buttonClicked(event)">${game[1][1]}</button>
+                <button id="12" onClick = "buttonClicked(event)">${game[1][2]}</button>
             </div>
             <div class="w-50 text-center">
-                <button>O</button>
-                <button>-</button>
-                <button>X</button>
+                <button id="20" onClick = "buttonClicked(event)">${game[2][0]}</button>
+                <button id="21" onClick = "buttonClicked(event)">${game[2][1]}</button>
+                <button id="22" onClick = "buttonClicked(event)">${game[2][2]}</button>
             </div>
         </div>
     `

573e2f6933522f5a1b59897af867c664981c3466
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2019-10-26 10:20:24 -0500

    removing comment

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -19,7 +19,6 @@ function buttonClicked(event) {
     let rowIdx = clickedButtonID[0]
     let colIdx = clickedButtonID[1]
     renderSquare(rowIdx, colIdx)
-    // comment for Juan
 }
 
 function hasSomeoneWon() {
